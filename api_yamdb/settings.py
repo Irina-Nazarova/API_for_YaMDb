3600923311efd65facc935815b0d68741d5f550c
Irina-Nazarova <Irina.g.nazarova@yandex.ru>
2021-02-09 18:33:05 +0300

    test workflows

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -85,18 +85,19 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+DJANGO_VAL = 'django.contrib.auth.password_validation.'
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': f'{DJANGO_VAL}UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': f'{DJANGO_VAL}.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': f'{DJANGO_VAL}.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': f'{DJANGO_VAL}.NumericPasswordValidator',
     },
 ]
 
